71bb2469a8ce41ca69187a86b151a4282b29100c
Shyam Saladi <saladi@caltech.edu>
2016-09-03 13:31:46 -0700

    remove rho as global variable. regression passing:
    ➜  cat test/test1.fna.faa | parallel  -N1 --recstart '>' --pipe -k src/RONN > test/test1.RONN32
    ➜  diff test/test1.RONN32 test1.fna.faa_new
    ➜  rm test1.fna.faa_new

--- src/callBBF.cpp
+++ src/callBBF.cpp
@@ -13,41 +13,45 @@ using namespace std;
 
 string query;
 vector<double> w;
-double		mu[2], sigma[2], disorder_weight;
 
 map < map <int, int>, double > yBar;
 
+// An store parameters for each model
+double mu[2], sigma[2], disorder_weight;
+
 vector< vector<short> >	dbAA;
+
 vector<short> seqAA;
 vector<int>	Length;
 vector<int> predictTimes;
 
-int		nD; //number of database sequences
-int		nW, rho0, rho1;
-//FILE		*fp;
-//                 A b C D E F G H I j K L M  N  o P  Q  R  S  T  u V  W  x Y
-int		INDEX[25]={0,0,1,2,3,4,5,6,7,0,8,9,10,11,0,12,13,14,15,16,0,17,18,0,19};
-int		Dayhoff[20][20]={
-{ 40, 24, 32, 32, 16, 36, 28, 28, 28, 24, 28, 32, 36, 32, 24, 36, 36, 32,  8, 20},
-{ 24, 80, 12, 12, 16, 20, 20, 24, 12,  8, 12, 16, 20, 12, 16, 32, 24, 24,  0, 32},
-{ 32, 12, 48, 44,  8, 36, 36, 24, 32, 16, 20, 40, 28, 40, 28, 32, 32, 24,  4, 16},
-{ 32, 12, 44, 48, 12, 32, 36, 24, 32, 20, 24, 36, 28, 40, 28, 32, 32, 24,  4, 16},
-{ 16, 16,  8, 12, 68, 12, 24, 36, 12, 40, 32, 16, 12, 12, 16, 20, 20, 28, 32, 60},
-{ 36, 20, 36, 32, 12, 52, 24, 20, 24, 16, 20, 32, 28, 28, 20, 36, 32, 28,  4, 12},
-{ 28, 20, 36, 36, 24, 24, 56, 24, 32, 24, 24, 40, 32, 44, 40, 28, 28, 24, 20, 32},
-{ 28, 24, 24, 24, 36, 20, 24, 52, 24, 40, 40, 24, 24, 24, 24, 28, 32, 48, 12, 28},
-{ 28, 12, 32, 32, 12, 24, 32, 24, 52, 20, 32, 36, 28, 36, 44, 32, 32, 24, 20, 16},
-{ 24,  8, 16, 20, 40, 16, 24, 40, 20, 56, 48, 20, 20, 24, 20, 20, 24, 40, 24, 28},
-{ 28, 12, 20, 24, 32, 20, 24, 40, 32, 48, 56, 24, 24, 28, 32, 24, 28, 40, 16, 24},
-{ 32, 16, 40, 36, 16, 32, 40, 24, 36, 20, 24, 40, 28, 36, 32, 36, 32, 24, 16, 24},
-{ 36, 20, 28, 28, 12, 28, 32, 24, 28, 20, 24, 28, 56, 32, 32, 36, 32, 28,  8, 12},
-{ 32, 12, 40, 40, 12, 28, 44, 24, 36, 24, 28, 36, 32, 48, 36, 28, 28, 24, 12, 16},
-{ 24, 16, 28, 28, 16, 20, 40, 24, 44, 20, 32, 32, 32, 36, 56, 32, 28, 24, 40, 16},
-{ 36, 32, 32, 32, 20, 36, 28, 28, 32, 20, 24, 36, 36, 28, 32, 40, 36, 28, 24, 20},
-{ 36, 24, 32, 32, 20, 32, 28, 32, 32, 24, 28, 32, 32, 28, 28, 36, 44, 32, 12, 20},
-{ 32, 24, 24, 24, 28, 28, 24, 48, 24, 40, 40, 24, 28, 24, 24, 28, 32, 48,  8, 24},
-{  8,  0,  4,  4, 32,  4, 20, 12, 20, 24, 16, 16,  8, 12, 40, 24, 12,  8,100, 32},
-{ 20, 32, 16, 16, 60, 12, 32, 28, 16, 28, 24, 24, 12, 16, 16, 20, 20, 24, 32, 72}};
+//nD: number of database sequences
+int	nD, nW;
+
+//               A b C D E F G H I j K L M  N  o P  Q  R  S  T  u V  W  x Y
+int	INDEX[25] = {0,0,1,2,3,4,5,6,7,0,8,9,10,11,0,12,13,14,15,16,0,17,18,0,19};
+int	Dayhoff[20][20] = {
+{40, 24, 32, 32, 16, 36, 28, 28, 28, 24, 28, 32, 36, 32, 24, 36, 36, 32,  8, 20},
+{24, 80, 12, 12, 16, 20, 20, 24, 12,  8, 12, 16, 20, 12, 16, 32, 24, 24,  0, 32},
+{32, 12, 48, 44,  8, 36, 36, 24, 32, 16, 20, 40, 28, 40, 28, 32, 32, 24,  4, 16},
+{32, 12, 44, 48, 12, 32, 36, 24, 32, 20, 24, 36, 28, 40, 28, 32, 32, 24,  4, 16},
+{16, 16,  8, 12, 68, 12, 24, 36, 12, 40, 32, 16, 12, 12, 16, 20, 20, 28, 32, 60},
+{36, 20, 36, 32, 12, 52, 24, 20, 24, 16, 20, 32, 28, 28, 20, 36, 32, 28,  4, 12},
+{28, 20, 36, 36, 24, 24, 56, 24, 32, 24, 24, 40, 32, 44, 40, 28, 28, 24, 20, 32},
+{28, 24, 24, 24, 36, 20, 24, 52, 24, 40, 40, 24, 24, 24, 24, 28, 32, 48, 12, 28},
+{28, 12, 32, 32, 12, 24, 32, 24, 52, 20, 32, 36, 28, 36, 44, 32, 32, 24, 20, 16},
+{24,  8, 16, 20, 40, 16, 24, 40, 20, 56, 48, 20, 20, 24, 20, 20, 24, 40, 24, 28},
+{28, 12, 20, 24, 32, 20, 24, 40, 32, 48, 56, 24, 24, 28, 32, 24, 28, 40, 16, 24},
+{32, 16, 40, 36, 16, 32, 40, 24, 36, 20, 24, 40, 28, 36, 32, 36, 32, 24, 16, 24},
+{36, 20, 28, 28, 12, 28, 32, 24, 28, 20, 24, 28, 56, 32, 32, 36, 32, 28,  8, 12},
+{32, 12, 40, 40, 12, 28, 44, 24, 36, 24, 28, 36, 32, 48, 36, 28, 28, 24, 12, 16},
+{24, 16, 28, 28, 16, 20, 40, 24, 44, 20, 32, 32, 32, 36, 56, 32, 28, 24, 40, 16},
+{36, 32, 32, 32, 20, 36, 28, 28, 32, 20, 24, 36, 36, 28, 32, 40, 36, 28, 24, 20},
+{36, 24, 32, 32, 20, 32, 28, 32, 32, 24, 28, 32, 32, 28, 28, 36, 44, 32, 12, 20},
+{32, 24, 24, 24, 28, 28, 24, 48, 24, 40, 40, 24, 28, 24, 24, 28, 32, 48,  8, 24},
+{ 8,  0,  4,  4, 32,  4, 20, 12, 20, 24, 16, 16,  8, 12, 40, 24, 12,  8,100, 32},
+{20, 32, 16, 16, 60, 12, 32, 28, 16, 28, 24, 24, 12, 16, 16, 20, 20, 24, 32, 72}};
+
 int Blosum62[20][20]={
 { 4, 0,-2,-1,-2, 0,-2,-1,-1,-1,-1,-1,-1,-1,-1, 1,-1,-2,-3,-2},
 { 0, 9,-3,-4,-2,-3,-3,-1,-3,-1,-1,-3,-3,-3,-3,-1,-1,-1,-2,-2},
@@ -70,6 +74,11 @@ int Blosum62[20][20]={
 {-3,-2,-4,-3, 1,-2,-2,-3,-3,-2,-1,-4,-4,-2,-3,-3,-3,-3,11, 2},
 {-2,-2,-3,-2, 3,-3, 2,-1,-2,-1,-1,-2,-3,-1,-2,-2,-2,-1, 2, 7}};
 
+// static void read_models(string fn_prefix, int model_count) {
+// 	for(int i=0; i < model_count; i++) {
+// 		model(fn_prefix)
+// 	}
+// }
 
 void model(string filename)
 {
@@ -137,7 +146,7 @@ void model(string filename)
 	 // Length[j]=str.length();
 
 	  //for(int r=0;r<strlen(str);r++)
-	//	dbAA[j][r]=INDEX[(int)(str[r]-'A')];
+	//	mydbAA[j][r]=INDEX[(int)(str[r]-'A')];
 }
 
 void pdf(string filename)
@@ -181,51 +190,56 @@ void pdf(string filename)
 }
 
 
-void align(int i,int j)//i for query sequence index and j for database sequence index
+void align(int i, int j, int rho[])
+// i for query sequence index
+// j for database sequence index
 {
-	int r, w, R, score;
-	rho1 = -1000000;
+	int R, score;
+	rho[0] = 0;
+	rho[1] = -1000000;
 
 	//go though the database sequence for maximised alignment
-	for(r = 0; r <= Length[j]-nW; r++)
+	for(int r = 0; r <= Length[j]-nW; r++)
 	{
 		score = 0;
 
 		//go through the query sequence for one alignment
-		for(w = 0; w < nW; w++)
+		for(int w = 0; w < nW; w++)
 			score += Blosum62[seqAA[i+w]][dbAA[j][r+w]];
 
-		if(score > rho1)
+		if(score > rho[1])
 		{
-			rho1 = score;
+			rho[1] = score;
 			R = r;
 		}
 	}
 
-	rho0= 0;
-	for(w = 0; w < nW; w++)
-		rho0 += Blosum62[dbAA[j][R+w]][dbAA[j][R+w]];
+	for(int w = 0; w < nW; w++)
+		rho[0] += Blosum62[dbAA[j][R+w]][dbAA[j][R+w]];
+
+	return;
 }
 
 
 void detect(vector<double> & estimate)
 {
 	double y, fOrder, fDisor, pOrder, pDisor;
+	int rho[2];
 
 	//string estimate_filename = "estimate.rec";
 
 	for(int i = 0; i < query.length(); i++)
-	{
 		predictTimes.push_back(0);
-	}
 
 	for(int i = 0; i <= query.length()-nW; i++)
 	{
 		y = 0.0;
 		for(int j = 0; j<nD; j++)
 		{
-			align(i, j); //search for the maximum alignment between ith peptide from the query and the jth database sequence
-			y += w[j] * exp((double)(rho1-rho0)/(double)rho0);
+			// search for the maximum alignment between ith query peptide
+			// jth database sequence
+			align(i, j, rho);
+			y += w[j] * exp((double)(rho[1]-rho[0])/(double)rho[0]);
 		}
 
 		fOrder = exp(-0.5*pow(y-mu[0],2.0)/sigma[0])/(sqrt(M_2_PI*sigma[0])); //$VR$: bug fixed by Ron in Feb07
@@ -295,7 +309,7 @@ int callBBF_driver(string myquery, string mod_fn, string pdf_fn1, double d_weigh
 		seqAA.push_back(INDEX[(int)(query[i]-'A')]);
 		if(seqAA[i]<0 || seqAA[i]>19)
 		{
-			printf("seqAA[%d]=%d\n",i,seqAA[i]);
+			printf("seqAA[%d]=%d\n", i, seqAA[i]);
 			return(1);
 		}
 	}
@@ -319,32 +333,3 @@ int callBBF_driver(string myquery, string mod_fn, string pdf_fn1, double d_weigh
 
 	return 0;
 }
-
-/*
-int main(int argc,char **argv)
-{
-int i;
-if(argc<4)
-  { printf("Command line is <callBBF sequence_fn model_fn pdf_fn disorder_weight>\n"); return(1); }
-if(argc==5) sscanf(argv[4],"%f",&disorder_weight);
-if(!(fp=fopen(argv[1],"r"))) { printf("Can't open %s\n",argv[1]); return(1); }
-int tmp1 = fscanf(fp,"%s",query);
-fclose(fp);
-//printf("callBBF: %s\n",query);
-for(i=0;i<strlen(query);i++)
-  {
-  seqAA[i]=INDEX[(int)(query[i]-'A')];
-  if(seqAA[i]<0 || seqAA[i]>19) { printf("seqAA[%d]=%d\n",i,seqAA[i]); exit(1); }
-  }
-model(argv[2]);
-printf("Model Worked\n");
-fflush(stdout);
-pdf(argv[3]);
-printf("PDF Worked\n");
-fflush(stdout);
-detect();
-printf("Detect Worked\n");
-fflush(stdout);
-return(1);
-}
-*/
